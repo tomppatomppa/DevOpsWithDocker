376b60303fda9d49749235dce313c9d09dc72835
tomi <tomiwest@hotmail.com>
2023-11-20 10:12:35 +0200

    EX.3.1

--- part3/EX.3.1-3.4/express-app/index.js
+++ part3/EX.3.1-3.4/express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World Update removed!</h1>')
+  res.send('<h1>Hello World TEST</h1>')
 })
 
 const PORT = 8080
